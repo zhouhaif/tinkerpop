fa34e3fbebed8d1621717742ed18e9bdcc7066ac
zhaoxinglin <zhaoxl@bsfit.com.cn>
2019-07-09 14:13:25 +0800

    fix shortestpath method name in __ file

--- gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/dsl/graph/__.java
+++ gremlin-core/src/main/java/org/apache/tinkerpop/gremlin/process/traversal/dsl/graph/__.java
@@ -304,7 +304,7 @@ public class __ {
     public static <A> GraphTraversal<A, Path> path() {
         return __.<A>start().path();
     }
-    public static <A> GraphTraversal<A, Path> shortestPath(final Long toIds,final Integer distance){return __.<A>start().shortestPathTransaction(toIds, distance);}
+    public static <A> GraphTraversal<A, Path> shortestPathTransaction(final Long toIds,final Integer distance){return __.<A>start().shortestPathTransaction(toIds, distance);}
     /**
      * @see GraphTraversal#match(Traversal[])
      */
